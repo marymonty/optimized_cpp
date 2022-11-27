efea212d0db0074b14464ef79373e9bdc70f7a4b
Mary Montgomery <mary.montgomery31@gmail.com>
2022-11-26 22:09:08 -0600

    updating F FIX comment

--- Basics/Debugging/F/F.cpp
+++ Basics/Debugging/F/F.cpp
@@ -18,10 +18,12 @@
 int FindBitsInNibble(unsigned int data)
 {
 	int n(0);
+	// FIX:
+	// two numbers were missing in the bitPattern (figured out below)
 	// the bitPattern signifies the number of 1's in the binary representation
 	// 1 = 0000 0001 (= 1 num bit)
 	// 4 = 0000 0100 (= 1 num bit)
-	// 7 = 0000 0111 (= 3 num bits <- needs to be corrected
+	// 7 = 0000 0111 = 3 num bits <- needs to be corrected
 	// 14 is the other correction - it was missing - needed a 3 at index 14
 	int bitPattern[NUM_BITS] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
 
