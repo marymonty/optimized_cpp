f94e05225cae96fd89a5267c49823c432d807929
Mary Montgomery <mary.montgomery31@gmail.com>
2022-12-01 10:36:58 -0600

    added FIX comment in J.cpp

--- Basics/Debugging/J/J.cpp
+++ Basics/Debugging/J/J.cpp
@@ -20,6 +20,9 @@ Dog::Dog(int val)
 	*pVal = val;
 }
 
+/* FIX : declare a copy constructor
+ * 	make sure a new pointer is correctly created and copied for pVal
+ */
 Dog::Dog(const Dog& d)
 {
 	pVal = new int;
